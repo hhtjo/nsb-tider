c3a3591fb1183e06d1bc506f217370503869bb1d
Hans <hahotjo>
2020-10-16 10:47:31 +0200

    Added vue view

---
+++ vue-gui/src/App.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+div.has-text-centered
+  h1.title.is-1 Ankomst tog Stavanger - Kristiansand
+  h2.title.is-3 {{todayDate}}
+  .columns
+    tid-item(title="NSB" :time="togTider.nsb")
+    tid-item(title="BaneNOR" :time="togTider.bNor")
+
+</template>
+
+<script>
+import TidItem from './components/tidItem'
+const axios = require('axios')
+
+export default {
+  name: 'app',
+  components: {
+    TidItem
+  },
+  data: function () {
+    return {
+      togTider: {}
+    }
+  },
+  created: function() {
+    axios.get('./main.js').then((response) => {
+      this.togTider = response.data
+    });
+  },
+  computed: {
+    todayDate: function () {
+      let d = new Date();
+      return `${d.getDate()}.${d.getMonth()}.${d.getFullYear()}`
+    }
+  },
+}
+</script>
+
+<style>
+h1 {
+  margin: 3rem 1rem;
+}
+</style>
---
+++ vue-gui/src/components/tidItem.vue
@@ -0,0 +1,49 @@
+<template lang="pug">
+.column
+    .card(style="margin:20px")
+        .card-header(style="padding:10px")
+            h2.title {{title}}
+        .card-content
+            button.button.is-loading.is-large(v-if="loading" style="border: none")
+            .columns(v-else)
+                .column 
+                    h5.title.is-5 Planlagt tid
+                    h3.green.title.is-3 {{time.tid}}
+                .column
+                    h5.title.is-5 Ny tid
+                    h3.red.title.is-3 {{time.nyTid}}
+</template>
+
+<script>
+export default {
+    name: 'TidItem',
+    props: ['title', 'time'],
+    data: function () {
+        return {
+            loading: true
+        }
+    },
+    watch: {
+        time: function(newTime) {
+            if(newTime !== undefined){
+                this.loading = false
+            } 
+        }
+    }
+}
+</script>
+
+<style >
+.card {
+}
+h3,h5 {
+    margin: 1rem;
+}
+.red {
+    color: red;
+}
+.green {
+    color: green;
+}
+</style>
+
